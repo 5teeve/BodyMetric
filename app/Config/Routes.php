1695d15b0132a0a9222be9b85e0b2c6f705dc23d
aina <herimahefasoaaina14@gmail.com>
2026-05-07 22:18:56 +0300

    update route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,38 @@
 <?php
 
-use CodeIgniter\Router\RouteCollection;
+namespace Config;
 
-/**
- * @var RouteCollection $routes
- */
-$routes->get('/', );
+// Create a new instance of our RouteCollection class.
+$routes = Services::routes();
+
+// Load the system's routing file first, so that the app and ENVIRONMENT
+// can override as needed.
+if (file_exists(SYSTEMPATH . 'Config/Routes.php')) {
+	require SYSTEMPATH . 'Config/Routes.php';
+}
+
+// Router Setup
+$routes->setDefaultNamespace('App\Controllers');
+$routes->setDefaultController('Home');
+$routes->setDefaultMethod('index');
+$routes->setTranslateURIDashes(false);
+$routes->set404Override();
+$routes->setAutoRoute(true);
+
+// Application Routes
+$routes->get('/', 'Home::index');
+$routes->get('welcome', 'Home::index');
+
+// Routes to render view files directly
+$routes->get('profil', static function () {
+	return view('profil/profil');
+});
+
+$routes->get('inscription/step1', static function () {
+	return view('inscription/register_step1');
+});
+
+$routes->get('inscription/step2', static function () {
+	return view('inscription/register_step2');
+});
+
